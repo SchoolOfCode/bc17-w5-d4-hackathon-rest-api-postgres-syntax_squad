6551a006c10091edd924bf76ceddc3b68c91cfd4
Mahik <mahik09@outlook.com>
2024-08-01 18:15:40 +0100

    GET artist, GET artist by id and POST id requests and helper functions completed

--- album.js
+++ album.js
@@ -1,23 +1,22 @@
-
 // Import the 'pool' object so our helper functions can interact with the PostgreSQL database
 import { pool } from "./db/index.js";
 
-export async function getResourceOne() {
-  // Query the database and return all resource ones
+export async function getAlbum() {
+  // Query the database and return all resource twos
 }
 
-export async function getResourceOneById(id) {
+export async function getAlbumById(id) {
   // Query the database and return the resource with a matching id or null
 }
 
-export async function createResourceOne(resource) {
+export async function createAlbum(album) {
   // Query the database to create an resource and return the newly created resource
 }
 
-export async function updateResourceOneById(id, updates) {
+export async function updateAlbumById(id, updates) {
   // Query the database to update the resource and return the newly updated resource or null
 }
 
-export async function deleteResourceOneById(id) {
+export async function deleteAlbumById(id) {
   // Query the database to delete the resource and return the deleted resource or null
 }
--- app.js
+++ app.js
@@ -1,28 +1,24 @@
 // Import the required modules
 import express from "express";
-
-
+// import { getArtist } from "./artist";
 
 // Import your helper functions for your first resource here
-// import {
-//   getResourceOne,
-//   getResourceOneById,
-//   createResourceOne,
-//   updateResourceOneById,
-//   deleteResourceOneById,
-// } from "./resource_one.js";
-
+import {
+  getArtist,
+  getArtistById,
+  createArtist,
+  updateArtistById,
+  deleteArtistById,
+} from "./artist.js";
 
 // Import your helper functions for your second resource here
 // import {
-//   getResourceTwo,
-//   getResourceTwoById,
-//   createResourceTwo,
-//   updateResourceTwoById,
-//   deleteResourceTwoById,
-// } from "./resource_two.js";
-
-
+//   getAlbum,
+//   getAlbumById,
+//   createAlbum,
+//   updateAlbumById,
+//   deleteAlbumById,
+// } from "./album.js";
 
 // Initialize the express app
 const app = express();
@@ -31,63 +27,67 @@ const PORT = process.env.PORT;
 
 app.use(express.json()); // express.json() middleware is used to parse incoming JSON requests
 
-
-
-
 // Resource One Route Handlers
 
-// Endpoint to retrieve all <resource_one>
-app.get("/resourceone/", async function (req, res) {
-    console.log("I'm alive");
-    res.status(200).send("I'm alive!");
+// Endpoint to retrieve all artists
+app.get("/artists/", async function (req, res) {
+  const artist = await getArtist();
+  res.status(200).json({ status: "success", data: artist });
 });
 
-// Endpoint to retrieve a <resource_one> by id
-app.get("/resourceone/:id", async function (req, res) {
+// Endpoint to retrieve a artist by id
+app.get("/artists/:id", async function (req, res) {
+  const id = req.params.id;
+  const artist = await getArtistById(id);
+  // Assume 404 status if the artist is not found
+  if (!artist) {
+    return res
+      .status(404)
+      .json({ status: "fail", data: { message: "Artist not found" } });
+  }
+  res.status(200).json({ status: "success", data: artist });
 });
 
-// Endpoint to create a new <resource_one>
-app.post("/resourceone/", async function (req, res) {
+// Endpoint to create a new artist
+app.post("/artists/", async function (req, res) {
+  try {
+    const data = req.body;
+    const artist = await createArtist(data);
+    res.status(201).json({ status: "success", data: artist });
+  } catch (err) {
+    res.status(404).json({
+      success: false,
+      payload: {
+        error: "That is an invalid input, please enter the Artist name",
+      },
+    });
+  }
 });
 
-// Endpoint to update a specific <resource_one> by id
-app.patch("/resourceone/:id", async function (req, res) {
-});
+// Endpoint to update a specific artist by id
+app.patch("/artists/:id", async function (req, res) {});
 
-// Endpoint to delete a specific <resource_one> by id
-app.delete("/resourceone/:id", async function (req, res) {
-});
-
-
-
+// Endpoint to delete a specific artist by id
+app.delete("/artists/:id", async function (req, res) {});
 
 // Resource Two Route Handlers
 
-// Endpoint to retrieve all <resource_twos>
-app.get("/resourcetwo/", async function (req, res) {
-    const authors = await getAuthors();
-    res.status(200).json({ status: "success", data: authors });
-  });
-  
-  // Endpoint to retrieve a <resource_twos> by id
-  app.get("/resourcetwo/:id", async function (req, res) {
-  });
-  
-  // Endpoint to create a new <resource_twos>
-  app.post("/resourcetwo/", async function (req, res) {
-  });
-  
-  // Endpoint to update a specific <resource_twos> by id
-  app.patch("/resourcetwo/:id", async function (req, res) {
-  });
-  
-  // Endpoint to delete a specific <resource_twos> by id
-  app.delete("/resourcetwo/:id", async function (req, res) {
-  });
+// Endpoint to retrieve all album
+app.get("/albums/", async function (req, res) {
+  res.status(200).json({ status: "success", data: authors });
+});
 
+// Endpoint to retrieve a album by id
+app.get("/albums/:id", async function (req, res) {});
 
+// Endpoint to create a new album
+app.post("/albums/", async function (req, res) {});
 
+// Endpoint to update a specific album by id
+app.patch("/albums/:id", async function (req, res) {});
 
+// Endpoint to delete a specific album by id
+app.delete("/albums/:id", async function (req, res) {});
 
 // Start the server and listen on the specified port
 app.listen(PORT, function () {
